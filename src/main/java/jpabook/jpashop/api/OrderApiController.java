b053137a920f2c8981f70438edc6e04a77c35162
Inu Jung <inust33@mobility42.io>
2022-12-27 13:15:53 +0900

    ..

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.dto.OrderSearch;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import jpabook.jpashop.service.OrderService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ public class OrderApiController {
 
     private final OrderService orderService;
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
     @GetMapping("/api/v2/orders")
     public List<OrderDto> orders(){
         return orderService.findOrders(new OrderSearch()).stream().map(OrderDto::new).collect(Collectors.toList());
@@ -39,6 +42,10 @@ public class OrderApiController {
 
         List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);//ToOne 관계만 fetch join
         return orders.stream().map(OrderDto::new).collect(Collectors.toList());
+    }
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDto();
     }
 
     @Getter
